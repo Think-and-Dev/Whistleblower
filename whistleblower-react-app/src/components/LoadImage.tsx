8c068a0cb63a00a0993491f5b00d765e7dc54cea
Mercedes <mercedes@thinkanddev.com>
2023-12-11 17:17:18 -0300

    Update LoadImage.tsx

--- whistleblower-react-app/src/components/LoadImage.tsx
+++ whistleblower-react-app/src/components/LoadImage.tsx
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
 import HandleUpload from "../utils/HandleUpload";
-import {
-  Button,
-  Container,
-  Typography,
-  Paper,
-  CircularProgress,
-} from "@mui/material";
+import { Button, Typography, Paper, CircularProgress } from "@mui/material";
 import ShowResult from "./ShowResult";
 
 export default function ImageLoad() {
@@ -17,6 +11,8 @@ export default function ImageLoad() {
   const [loading, setLoading] = useState<boolean>(false); // Nuevo estado para la carga
   const [processingMessage, setProcessingMessage] = useState<string>(""); // Nuevo estado para el mensaje de procesamiento
   const [showOutput, setShowOutput] = useState<boolean>(false); // Nuevo estado para mostrar el componente Output
+  const [showSelectImageButton, setShowSelectImageButton] =
+    useState<boolean>(true);
 
   const onDrop = (acceptedFiles: File[]) => {
     const file = acceptedFiles[0];
@@ -27,16 +23,16 @@ export default function ImageLoad() {
 
   const handleClick = async () => {
     try {
-      console.log("Entre al try");
       setLoading(true);
-      setProcessingMessage("Esto puede llevar algunos minutos...");
+      setProcessingMessage("This may take a few minutes...");
       const fileContent = await readFileAsArrayBuffer(selectedFile);
       const bytes = new Uint8Array(fileContent);
 
       HandleUpload(bytes);
       console.log("Archivo enviado con éxito");
 
-      setShowOutput(true); // Muestra el componente Output después de obtener el resultado
+      setShowOutput(false); // Muestra el componente Output después de obtener el resultado
+      setShowSelectImageButton(false);
     } catch (error) {
       console.error("Error al enviar el archivo", error);
     } finally {
@@ -72,25 +68,26 @@ export default function ImageLoad() {
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
 
   return (
-    <Container component="main" maxWidth="xs" style={{ marginTop: "20px" }}>
-      <Paper
-        elevation={3}
-        style={{
-          padding: "20px",
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
+    <Paper
+      elevation={3}
+      style={{
+        padding: "20px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        background: "rgba(255, 255, 255, 0.8)",
+      }}
+    >
+      <Typography
+        variant="h5"
+        align="center"
+        style={{ marginTop: "20px", marginBottom: "20px", color: "#7f69a5" }}
       >
-        <Typography
-          variant="h5"
-          align="center"
-          style={{ marginTop: "20px", marginBottom: "20px", color: "#7f69a5" }}
-        >
-          WHISTLEBLOWER
-        </Typography>
-        <div {...getRootProps()}>
-          <input {...getInputProps()} />
+        WHISTLEBLOWER
+      </Typography>
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {showSelectImageButton && (
           <Button
             variant="outlined"
             component="span"
@@ -102,47 +99,52 @@ export default function ImageLoad() {
           >
             Select Image
           </Button>
+        )}
+      </div>
+      {imagen && (
+        <div style={{ marginTop: "20px" }}>
+          <img
+            src={imagen}
+            alt=""
+            style={{
+              maxWidth: "100%",
+              maxHeight: "300px",
+              border: "4px solid #414141",
+              borderRadius: "5px",
+            }}
+          />
         </div>
-        {imagen && (
-          <div style={{ marginTop: "20px" }}>
-            <Typography variant="body1">Image loaded:</Typography>
-            <img
-              src={imagen}
-              alt=""
-              style={{
-                maxWidth: "100%",
-                maxHeight: "300px",
-                border: "4px solid #414141",
-                borderRadius: "5px",
-              }}
-            />
-          </div>
-        )}
-        {loading && (
-          <div style={{ marginBottom: "10px" }}>
-            <CircularProgress
-              size={24}
-              color="secondary"
-              style={{ marginRight: "10px" }}
-            />
-            <Typography variant="body2">{processingMessage}</Typography>
-          </div>
-        )}
-        <Button
-          variant="contained"
-          color="secondary"
-          onClick={handleClick}
+      )}
+      {loading && (
+        <div
           style={{
-            marginTop: "20px",
-            backgroundColor: "#7f69a5",
-            color: "white",
+            marginBottom: "10px",
+            display: "flex",
+            alignItems: "center",
           }}
         >
-          Get plate!
-        </Button>
-        {showOutput && <ShowResult result={result} />}{" "}
-        {/* Muestra el componente Output si showOutput es true */}
-      </Paper>
-    </Container>
+          <CircularProgress
+            size={24}
+            color="secondary"
+            style={{ marginRight: "10px" }}
+          />
+          <Typography variant="body2">{processingMessage}</Typography>
+        </div>
+      )}
+      <Button
+        variant="contained"
+        color="secondary"
+        onClick={handleClick}
+        style={{
+          marginTop: "20px",
+          backgroundColor: "#7f69a5",
+          color: "white",
+        }}
+      >
+        Get plate!
+      </Button>
+      {showOutput && <ShowResult result={result} />}{" "}
+      {/* Muestra el componente Output si showOutput es true */}
+    </Paper>
   );
 }
