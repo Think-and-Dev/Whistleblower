e261bc57147b79ede51c8a76a8541f7b3a2e8bb2
Mercedes <mercedes@thinkanddev.com>
2023-12-19 14:27:01 -0300

    feat: add CropImage component

---
+++ whistleblower-react-app/src/components/CropImage.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+
+const CropImage: React.FC<{
+  image: string | undefined;
+  coordinates: number[];
+}> = ({ image, coordinates }) => {
+  const [croppedImage, setCroppedImage] = useState<string | undefined>(
+    undefined
+  );
+
+  useEffect(() => {
+    if (image) {
+      const img = new Image();
+      img.onload = () => {
+        const canvas = document.createElement("canvas");
+        const ctx = canvas.getContext("2d");
+        if (!ctx) {
+          console.error("Unable to get 2D context for canvas");
+          return;
+        }
+
+        const [x, y, width, height] = coordinates;
+        canvas.width = width;
+        canvas.height = height;
+
+        // Dibujar la porción recortada de la imagen en el lienzo
+        ctx.drawImage(img, x, y, width, height, 0, 0, width, height);
+
+        // Obtener la URL de la imagen recortada desde el lienzo
+        const croppedImageUrl = canvas.toDataURL("image/jpeg");
+
+        setCroppedImage(croppedImageUrl);
+      };
+
+      img.src = image;
+    }
+  }, [image, coordinates]);
+
+  return (
+    <div style={{ marginTop: "20px" }}>
+      {croppedImage && (
+        <img
+          src={croppedImage}
+          alt="Cropped Image"
+          style={{
+            maxWidth: "100%",
+            maxHeight: "300px",
+            border: "4px solid #414141",
+            borderRadius: "5px",
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default CropImage;
